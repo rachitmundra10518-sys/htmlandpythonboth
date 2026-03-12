697101c6e0980833fcb4b4321a246c155ca4a912
rachitmundra10518-sys <rachitmundra10518@gmail.com>
2026-03-13 02:14:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    
+    result = None
+    
+    if request.method == "POST":
+        num1 = int(request.form["num1"])
+        num2 = int(request.form["num2"])
+        result = num1 + num2
+
+    return render_template("index.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
